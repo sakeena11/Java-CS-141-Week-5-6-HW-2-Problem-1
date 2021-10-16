716f5dc28306a326fc8ecb0e14a89dea9828dcf5
sakeena11 <88400044+sakeena11@users.noreply.github.com>
2021-10-15 19:30:34 -0500

    Initial

---
+++ Average.java
@@ -0,0 +1,25 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Average {
+    public static void printMultiples(int n, int max) {
+        //TODO: complete this method
+
+        for (int i = 1; i <= max; i++) {
+            if (i % n == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+        public static void main (String[]args){
+            Scanner s = new Scanner(System.in);
+            System.out.println("Enter a positive integer:");
+            int n = s.nextInt();
+            System.out.println("Enter a maximum positive integer:");
+            int max = s.nextInt();
+            printMultiples(n, max);
+
+
+        }
+
+    }
